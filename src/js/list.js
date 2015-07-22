e5d9d181bd854eb939080ab539dd4e2b8de5943e
sisisin <azsisisin@gmail.com>
2015-07-23 00:16:11 +0900

    add button event

--- src/js/list.js
+++ src/js/list.js
@@ -1,14 +1,6 @@
 var React = require('react/addons');
 var $ = require('jquery');
 
-var userStatus = [
-		{
-		name:'sisisin',
-		term:'2015/1/1 ~ 2015/2/1',
-		state:'待ち'
-	}
-];
-
 var List = React.createClass({
 	getInitialState: function () {
 		return { userStatus: [] };
@@ -26,6 +18,27 @@ var List = React.createClass({
       }.bind(this)
     });	
 	},
+	handleRequestSubmit: function (term) {
+		var terms = this.state.userStatus;
+		var newTerms = terms.concat([term]);
+		this.setState({userStatus: newTerms});
+ 
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: term,
+			success: function(data) {
+			this.setState({userStatus: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+			console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	handleApproveSubmit: function () {
+		
+	},
 	componentDidMount: function () {
     this.loadUserStatusFromServer();
     setInterval(this.loadUserStatsFromServer, this.props.pollInterval);
@@ -33,29 +46,64 @@ var List = React.createClass({
 	render: function () {
 		return (
 			<div>
-				<form>
-					<select><option>1</option></select>年
-					<select><option>1</option></select>月
-					<select><option>1</option></select>日〜
-					<select><option>1</option></select>年
-					<select><option>1</option></select>月
-					<select><option>2</option></select>日まで
-					<input type="button" value="送信"></input>
-				</form>				
-				<UserList data={this.state.userStatus}/>				
+				<RequestForm onRequestSubmit={this.handleRequestSubmit}/>	
+				<UserList data={this.state.userStatus} handleApproveSubmit={this.handleApproveSubmit}/>				
 			</div>
+		);
+	}
+});
+
+var RequestForm = React.createClass({
+	handleSubmit: function(e) {
+		e.preventDefault();
+		var fromYear = React.findDOMNode(this.refs.fromYear).value.trim();
+		var fromMonth = React.findDOMNode(this.refs.fromMonth).value.trim();
+		var fromDay = React.findDOMNode(this.refs.fromDay).value.trim();
+		var toYear = React.findDOMNode(this.refs.toYear).value.trim();
+		var toMonth = React.findDOMNode(this.refs.toMonth).value.trim();
+		var toDay = React.findDOMNode(this.refs.toDay).value.trim();
+		if (!fromYear || !fromMonth) {
+		return;
+		}
+
+		this.props.onRequestSubmit({
+			name:'sisisin',
+			term:fromYear + '/' + fromMonth + '/' + fromDay  + ' ~ ' + toYear + '/' + toMonth  + '/' + toDay,
+			state:'待ち'
+		});
+		React.findDOMNode(this.refs.fromYear).value = '';
+		React.findDOMNode(this.refs.fromYear).value = '';
+		React.findDOMNode(this.refs.fromYear).value = '';
+		React.findDOMNode(this.refs.fromYear).value = '';
+		React.findDOMNode(this.refs.fromYear).value = '';
+		React.findDOMNode(this.refs.fromYear).value = '';
+		return;
+	},
+	render: function () {
+		return (
+			<form onSubmit={this.handleSubmit}>
+					<select ref="fromYear"><option>2015</option></select>年
+					<select ref="fromMonth"><option>1</option></select>月
+					<select ref="fromDay"><option>1</option></select>日〜
+					<select ref="toYear"><option>2015</option></select>年
+					<select ref="toMonth"><option>1</option></select>月
+					<select ref="toDay"><option>2</option></select>日まで
+					<input type="submit" value="送信"></input>
+				</form>	
 		);
 	}
 });
 
 var UserList = React.createClass({
 	render: function () {
-		var status = this.props.data.map(function(user) {
+		var handleApproveSubmit = this.props.handleApproveSubmit;
+		var status = this.props.data.map(function(user, i) {
 			return (
 				<tr>
 					<td>{user.name}</td>
 					<td>{user.term}</td>
 					<td>{user.state}</td>
+					<td><UserRequestApprove userId={i} handleApproveSubmit={handleApproveSubmit}/></td>
 				</tr>);
 		});
 		return (
@@ -64,13 +112,24 @@ var UserList = React.createClass({
 					<td>名前</td>
 					<td>期間</td>
 					<td>状態</td>
+					<td></td>
 				</tr>
 				{status}
 			</table>
 		);
 	}
-});
+}).bind(this);
 
+var UserRequestApprove = React.createClass({
+	handleSubmit: function () {
+		this.props.handleApproveSubmit(this.props.userId);
+	},
+	render: function () {
+		return (
+			<form onSubmit={this.handleSubmit}><input type="submit" value="承認" /></form>
+		);
+	}
+});
 
 React.render(
 	<List url="/js/data.json" pollInterval={2000} />,
